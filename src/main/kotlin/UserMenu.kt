b9e98e99a546ec1a095030f4d17f81660b8fb8fc
mtamrod <trmario@iesrafaelalberti.es>
2024-04-03 18:03:20 +0200

    First Commit

--- src/main/kotlin/UserMenu.kt
+++ src/main/kotlin/UserMenu.kt
@@ -1,2 +1,26 @@
 class UserMenu {
+    interface IMenu {
+        fun mostrarMenu()
+        fun generarSerie()
+        fun jugarAhorcado()
+    }
+
+    fun mostrarMenu() {
+        do {
+            println("MENÚ:")
+            println("1. Generar serie")
+            println("2. Jugar al ahorcado:")
+            println("3. Salir")
+            println("Por favor, selecciona una opción: ")
+
+            val option = readln().toIntOrNull()
+
+            when (option) {
+                1 -> //generarserie
+                2 -> //jugarahorcado
+                3 -> println("Saliendo...")
+                else -> println("Opción no válida")
+            }
+        } while (option != 3)
+    }
 }
